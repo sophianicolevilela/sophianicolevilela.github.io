8b07b8ce3985c25e5183fdc1ab78c7801b3671e7
sophianicolevilela <97068923+sophianicolevilela@users.noreply.github.com>
2022-01-18 08:51:51 +0800

    My Button.javascript

--- javascript.js
+++ javascript.js
@@ -1,2 +1,5 @@
-// javascript 
-console.log ("")
+// javascript
+const btn = document.createElement('button');
+btn.innerText = 'New Button';
+document.body.appendChild(btn):
+<button>New Button</button>
